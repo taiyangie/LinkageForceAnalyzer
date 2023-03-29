6689799f26860e8f3ae907481c2cf8d0b72a1071
taiyangie <sunnyxli2019@gmail.com>
2023-03-28 22:33:02 -0400

    Force calculations for acceleration and braking

--- ForceCalcs.py
+++ ForceCalcs.py
@@ -28,42 +28,42 @@ c = 24.4 #28 #CG to rear axle
 b = 36.6 #32.125 #front axle to CG
 
 #-------------------------------------------------------
-# ## LINEAR ACCELERATION
-#
-# #Y
-# LinAccel_Fyfr = 0 #forces in y, front right
-# LinAccel_Fyfl = 0
-# LinAccel_Fyrr = 0
-# LinAccel_Fyrl = 0
-# #X
-# mu=.714
-# LinAccel_Fxrr = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
-# LinAccel_Fxrl = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
-# LinAccel_Fxfr = 0
-# LinAccel_Fxfl = 0
-# #Z
-# g = .9945
-# LinAccel_Fzfr = (W* ((c/L) - g*(h/L))) / 2
-# LinAccel_Fzfl = (W* ((c/L) - g*(h/L))) / 2
-# LinAccel_Fzrr = (W* ((b/L) + g*(h/L))) / 2 #div by 2 for ea wheel
-# LinAccel_Fzrl = (W* ((b/L) + g*(h/L))) / 2
-# #-------------------------------------------------------
+## LINEAR ACCELERATION
+
+#Y
+LinAccel_Fyfr = 0 #forces in y, front right
+LinAccel_Fyfl = 0
+LinAccel_Fyrr = 0
+LinAccel_Fyrl = 0
+#X
+mu=.714
+LinAccel_Fxrr = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
+LinAccel_Fxrl = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
+LinAccel_Fxfr = 0
+LinAccel_Fxfl = 0
+#Z
+g = .9945
+LinAccel_Fzfr = (W* ((c/L) - g*(h/L))) / 2
+LinAccel_Fzfl = (W* ((c/L) - g*(h/L))) / 2
+LinAccel_Fzrr = (W* ((b/L) + g*(h/L))) / 2 #div by 2 for ea wheel
+LinAccel_Fzrl = (W* ((b/L) + g*(h/L))) / 2
+#-------------------------------------------------------
 
 
-# ## BRAKING PERFORMANCE
-#
-# #Y
-# Fy = 0
-# #X
-# Fxr = 2* 2.7*(586.71/r)
-# Fxmf = (1.34 * (Wfs + (h/L)*Fxr)) / (1 - 1.34*(h/L))#6.97*(407.37/r)
-# Dx = (Fxmf + Fxr)/W #1.7, this is off by .1
-# Fxfront = 1.34* (Wfs + ((W*Dx*h)/L))
-#
-# Fxrear = 1.34 * (Wrs - ((W*Dx*h)/L)) #this is off by 2lb
-# #Z
-# Fzfront = Wfs + ((W*Dx*h)/L)
-# Fzrear = Wrs -((W*Dx*h)/L) #these are all slightly off from the paper :(
+## BRAKING PERFORMANCE
+
+#Y
+Fy = 0
+#X
+Fxr = 2* 2.7*(586.71/r)
+Fxmf = (1.34 * (Wfs + (h/L)*Fxr)) / (1 - 1.34*(h/L))#6.97*(407.37/r)
+Dx = (Fxmf + Fxr)/W #1.7, this is off by .1
+Fxfront = 1.34* (Wfs + ((W*Dx*h)/L))
+
+Fxrear = 1.34 * (Wrs - ((W*Dx*h)/L)) #this is off by 2lb
+#Z
+Fzfront = Wfs + ((W*Dx*h)/L)
+Fzrear = Wrs -((W*Dx*h)/L) #these are all slightly off from the paper :(
 
 #-------------------------------------------------------
 
