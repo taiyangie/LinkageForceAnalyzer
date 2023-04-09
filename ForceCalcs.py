6816fa8fafc8dd43866657486701d6aa9af5cee5
taiyangie <sunnyxli2019@gmail.com>
2023-04-09 16:58:11 -0400

    Relabeled Braking

--- ForceCalcs.py
+++ ForceCalcs.py
@@ -29,47 +29,57 @@ b = 36.6 #32.125 #front axle to CG
 
 #-------------------------------------------------------
 ## LINEAR ACCELERATION
-
-#Y
-LinAccel_Fyfr = 0 #forces in y, front right
-LinAccel_Fyfl = 0
-LinAccel_Fyrr = 0
-LinAccel_Fyrl = 0
-#X
-mu=.714
-LinAccel_Fxrr = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
-LinAccel_Fxrl = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
-LinAccel_Fxfr = 0
-LinAccel_Fxfl = 0
-#Z
-g = .9945
-LinAccel_Fzfr = (W* ((c/L) - g*(h/L))) / 2
-LinAccel_Fzfl = (W* ((c/L) - g*(h/L))) / 2
-LinAccel_Fzrr = (W* ((b/L) + g*(h/L))) / 2 #div by 2 for ea wheel
-LinAccel_Fzrl = (W* ((b/L) + g*(h/L))) / 2
+#
+# #Y
+# LinAccel_Fyfr = 0 #forces in y, front right
+# LinAccel_Fyfl = 0
+# LinAccel_Fyrr = 0
+# LinAccel_Fyrl = 0
+# #X
+# mu=.714
+# LinAccel_Fxrr = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
+# LinAccel_Fxrl = ((mu*W*b)/L) / (1-(h/L)*mu) / 2
+# LinAccel_Fxfr = 0
+# LinAccel_Fxfl = 0
+# #Z
+# g = .9945
+# LinAccel_Fzfr = (W* ((c/L) - g*(h/L))) / 2
+# LinAccel_Fzfl = (W* ((c/L) - g*(h/L))) / 2
+# LinAccel_Fzrr = (W* ((b/L) + g*(h/L))) / 2 #div by 2 for ea wheel
+# LinAccel_Fzrl = (W* ((b/L) + g*(h/L))) / 2
 #-------------------------------------------------------
 
 
 ## BRAKING PERFORMANCE
 
 #Y
-Fy = 0
+Brake_Fyfr = 0
+Brake_Fyfl = 0
+
+Brake_Fyrr = 0
+Brake_Fyrl = 0
 #X
 Fxr = 2* 2.7*(586.71/r)
 Fxmf = (1.34 * (Wfs + (h/L)*Fxr)) / (1 - 1.34*(h/L))#6.97*(407.37/r)
 Dx = (Fxmf + Fxr)/W #1.7, this is off by .1
-Fxfront = 1.34* (Wfs + ((W*Dx*h)/L))
 
-Fxrear = 1.34 * (Wrs - ((W*Dx*h)/L)) #this is off by 2lb
+Brake_Fxfr = (1.34* (Wfs + ((W*Dx*h)/L))) / 2
+Brake_Fxfl = (1.34* (Wfs + ((W*Dx*h)/L))) / 2
+
+Brake_Fxrr = (1.34 * (Wrs - ((W*Dx*h)/L))) / 2 #this is off by 2lb
+Brake_Fxrl = (1.34 * (Wrs - ((W*Dx*h)/L))) / 2
 #Z
-Fzfront = Wfs + ((W*Dx*h)/L)
-Fzrear = Wrs -((W*Dx*h)/L) #these are all slightly off from the paper :(
+Brake_Fzfr =( Wfs + ((W*Dx*h)/L)) /2
+Brake_Fzfl =( Wfs + ((W*Dx*h)/L)) /2
+
+Brake_Fzrr = (Wrs -((W*Dx*h)/L)) / 2 #these are all slightly off from the paper :(
+Brake_Fzrl = (Wrs -((W*Dx*h)/L)) / 2
 
 #-------------------------------------------------------
 
-## STEADY STATE CORNERING
-h1 = 12 #height of spring mass center of grav above roll axis
-hf = 3.03 #dist front acle and roll axis
-Kphif = 3219
-Kphir = 3663
-phi = (W*h1* (1)) / (Kphif+Kphir-W*h1) #replace 1 w number of gs the vehicle undergoes while cornering
+# ## STEADY STATE CORNERING
+# h1 = 12 #height of spring mass center of grav above roll axis
+# hf = 3.03 #dist front acle and roll axis
+# Kphif = 3219
+# Kphir = 3663
+# phi = (W*h1* (1)) / (Kphif+Kphir-W*h1) #replace 1 w number of gs the vehicle undergoes while cornering
